f53f73546b57edb1f3c6030147f94ebcfb997eda
gaohanghbut <568720503@qq.com>
2018-10-03 20:03:00 +0800

    注解扩展的使用说明

--- stupid-mybatis-core/src/main/java/cn/yxffcode/stupidmybatis/core/cfg/MapperConfigHandler.java
+++ stupid-mybatis-core/src/main/java/cn/yxffcode/stupidmybatis/core/cfg/MapperConfigHandler.java
@@ -6,8 +6,16 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 
 /**
+ * 处理注解配置的接口定义
  * @author gaohang
  */
 public interface MapperConfigHandler<T extends Annotation> {
+  /**
+   * 处理注解配置
+   * @param annotation 配置的注解
+   * @param type mapper接口
+   * @param method mapper接口上标了此注解的方法
+   * @param assistant 配置的builder工具
+   */
   void handleAnnotation(T annotation, Class<?> type, Method method, MapperBuilderAssistant assistant) throws Throwable;
 }
